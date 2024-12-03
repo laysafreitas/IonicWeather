b790c1aea06e1611eae0271f4db8c024c5a244a5
Leandro-Willams <leandrinho.trajano@gmail.com>
2024-12-03 14:43:52 -0300

    Modificando app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
- 
+
 ];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
-})
+}) ///
 export class AppRoutingModule {}
